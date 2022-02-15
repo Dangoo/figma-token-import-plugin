b04ce109891609818bcf4274147b73032a70cca6
dangoo <daniel.gooss@sinnerschrader.com>
2022-02-15 22:53:00 +0100

    refactor: rearrange file structure

--- src/importTokens.ts
+++ src/importTokens.ts
@@ -1,27 +1,14 @@
 import type { DesignToken } from 'style-dictionary';
+import { flattenObjectToArray } from './utils/flattenObjectToArray';
 import { importGridStyles } from './importGridStyles';
 import { importPaintStyles } from './importPaintStyles';
 import { DesignTokensByType, ImportPromise, isColor, isDesignToken, isSize } from './types';
-
-function flattenObjectToArray(obj: Record<string, unknown>, acc = []): DesignToken[] {
-  return Object.values(obj).reduce<DesignToken[]>(
-    (subAcc: DesignToken[], subObj: Record<string, unknown>) => {
-      if (isDesignToken(subObj)) {
-        subAcc.push(subObj);
-      } else {
-        flattenObjectToArray(subObj, subAcc);
-      }
-      return subAcc;
-    },
-    acc,
-  );
-}
 
 export function parsePayload(
   payload: Record<'color', Record<string, unknown>>,
 ): DesignTokensByType {
   return {
-    PAINT: flattenObjectToArray(payload.color),
+    PAINT: flattenObjectToArray(payload.color, isDesignToken),
   };
 }
 
